f3d59dc56f383e9203bf41246279debddda51596
RickardTimergalejevs <rickard.time@gmail.com>
2024-02-22 19:25:29 +0100

    Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import variables from "../styles/variables.module.scss";
 
 export default function Home() {
   return (
-    <main className={"r"} style={{ backgroundColor: variables.primaryColor }}>
-      <h1>Test</h1>
-    </main>
+    <>
+      <main className={"r"} style={{ backgroundColor: variables.primaryColor }}>
+        <h1>Test</h1>
+      </main>
+    </>
   );
 }
