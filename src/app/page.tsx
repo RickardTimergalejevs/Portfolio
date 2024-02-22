053f11dcbd25ef725febf1f9bcceefe29b5b057a
RickardTimergalejevs <rickard.time@gmail.com>
2024-02-22 18:08:24 +0100

    Next.js installation

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import variables from "../styles/variables.module.scss";
+
+export default function Home() {
+  return (
+    <main className={"r"} style={{ backgroundColor: variables.primaryColor }}>
+      <h1>Test</h1>
+    </main>
+  );
+}
